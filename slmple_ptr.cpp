a48aa1238de6fc4478f66b5bf41a41e404911803
20251814 <chji0911f@naver.com>
2026-05-13 13:58:17 +0900

    fin

---
+++ slmple_ptr.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int x = 5;
+	int y = 10;
+
+	//p는 x를 가리킴
+	int* p = &x;
+	int* q = &y;
+
+	*p = 6;
+	*q = *p * 2;
+	cout << "x = " << x << ", y= "<< y << endl;
+	cout << "x의 주소 = " << p << ", y의 주소 = " << q << endl;
+
+	
+	int a=1, b=2, c=3, d=4, e=5, f=6, g=7;
+	int* h=&a, *i, j, k, l, m, n;
+
+	cout << endl << "변수의 값: "
+		<< "a= " << a << endl
+		<< "b= " << b << endl
+		<< "c= " << b << endl
+		<< "d= " << b << endl
+		<< "e= " << b << endl
+		<< "f= " << b << endl
+		<< "g= " << b << endl;
+
+	cout << endl << "변수의 주소: "
+		<< "h= " << h << endl
+		<< "i= " << i << endl
+		<< "j= " << j << endl
+		<< "k= " << k << endl
+		<< "l= " << l << endl
+		<< "m= " << m << endl
+		<< "n= " << n << endl;
+		
+
+
+	
+
+
+
+
+	return 0;
+}
---
+++ write_args.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+// C++ 파일 실행할 수 있는 과정:
+// .cpp -> 전처리 -> 컴파일 ->
+// 오브젝트(wrie_agrs.obj / .o) ->
+// 링킹 -> 실행파일(write_args.exe / a.out)
+
+// 터미널에서 실행한다면:
+// >>> ./write_args.exe say hello world
+// argc = 3
+// argv = ["say", "hello", "world"]
+int main(int argc, char* argv) { // Linux 터미널에서 많이 사용할
+	// 명령 프롬포트에 입력한 인수가 존재하면 출력
+	if (argc > 1) { // argc = 인수의 개수 (argument count)
+		// 첫 번째 인수를 출력
+		// 공백 하나와 함께 나머지 인수들을 각각 출력
+		cout << "실행 파일: " << argv[0] << endl;
+
+		for (int i = 1; i < argc; i++) {
+
+			cout << argv[i] << "";
+		}
+		cout << endl;
+	}
+	return 0;
+}
